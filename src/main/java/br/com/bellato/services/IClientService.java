8504dbcbfc28f1f102ea11d5722215ed521c52d2
bryanbellato <brbhasegawa@gmail.com>
2024-08-12 01:35:16 -0300

    Data saving and search relacted-functionalities added on the interface and on the ClientTest. Main.java is now executable for our TDD purpose.

--- src/main/java/br/com/bellato/services/IClientService.java
+++ src/main/java/br/com/bellato/services/IClientService.java
@@ -1,4 +1,11 @@
 package main.java.br.com.bellato.services;
 
+import main.java.br.com.bellato.domain.Client;
+
 public interface IClientService {
+
+    void save(Client client);
+
+    Client searchByCPF(Long cpf);
+
 }
--- src/test/java/br/com/bellato/ClientTest.java
+++ src/test/java/br/com/bellato/ClientTest.java
@@ -1,12 +1,17 @@
 package test.java.br.com.bellato;
 
 import main.java.br.com.bellato.domain.Client;
+import main.java.br.com.bellato.services.IClientService;
 import org.junit.Test;
+import org.junit.Assert;
 
 public class ClientTest {
 
+    private IClientService clientService;
+
     @Test
     public void searchClient() {
+
         Client client = new Client();
         client.setCpf(123456789L);
         client.setName("Fulano");
@@ -14,5 +19,11 @@ public class ClientTest {
         client.setCity("Black Hole");
         client.setState("Somewhere in your nearest galaxy.");
         client.setPhone(40028922L);
+
+        clientService.save(client);
+
+        Client clientSearched = clientService.searchByCPF(client.getCpf());
+        Assert.assertNotNull(clientSearched);
+
     }
 }
