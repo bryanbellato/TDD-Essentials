e3378cd668c8097c91c9ae5bfb41c20243c0a073
bryanbellato <brbhasegawa@gmail.com>
2024-08-12 22:52:59 -0300

    Refactored 'ClientTest' to 'ClientServiceTest'. Implemented INIT structure on the test class.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
-import test.java.br.com.bellato.ClientTest;
+import test.java.br.com.bellato.ClientServiceTest;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Running ClientTest...");
 
-        Result result = JUnitCore.runClasses(ClientTest.class);
+        Result result = JUnitCore.runClasses(ClientServiceTest.class);
 
         for (Failure failure : result.getFailures()) {
             System.out.println(failure.toString());
--- src/test/java/br/com/bellato/ClientServiceTest.java
+++ src/test/java/br/com/bellato/ClientServiceTest.java
@@ -4,23 +4,24 @@ import main.java.br.com.bellato.dao.IClientDAO;
 import main.java.br.com.bellato.domain.Client;
 import main.java.br.com.bellato.services.ClientService;
 import main.java.br.com.bellato.services.IClientService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
 import test.java.br.com.bellato.dao.ClientDaoMock;
 
-public class ClientTest {
+public class ClientServiceTest {
 
     private IClientService clientService;
+    private Client client;
 
-    public ClientTest() {
+    public ClientServiceTest() {
         IClientDAO dao = new ClientDaoMock();
         clientService = new ClientService(dao);
     }
 
-    @Test
-    public void searchClient() {
-
-        Client client = new Client();
+    @Before
+    public void init() {
+        client = new Client();
         client.setCpf(123456789L);
         client.setName("Fulano");
         client.setAddress("Fim do Mundo");
@@ -29,9 +30,12 @@ public class ClientTest {
         client.setPhone(40028922L);
 
         clientService.save(client);
+    }
 
+    @Test
+    public void searchClient() {
         Client clientSearched = clientService.searchByCPF(client.getCpf());
         Assert.assertNotNull(clientSearched);
-
     }
+
 }
